e04a346d55ecc73fd0afe2fc34d9275c1eab6f6e
kathrynbower <kathryn.l.bower@gmail.com>
2016-11-21 20:44:36 -0500

    Scripts Update

---
+++ HeroCounter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroCounter : MonoBehaviour
+{
+	public Light PlayerLight;
+	public bool isDimming;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (isDimming) {
+			//PlayerLight.intensity = PlayerLight.intensity * (1 - Time.deltaTime);
+			PlayerLight.intensity -= Time.deltaTime;
+			if (PlayerLight.intensity <= 0) {
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+}
---
+++ PickUpObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpObject : MonoBehaviour
+{
+
+	public Light PlayerLight;
+	public bool isDimming;
+	public float maxIntensity;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Player")) {
+			Destroy (gameObject);
+			if (isDimming) {
+				PlayerLight.intensity = maxIntensity;
+			}
+		}
+	}
+}
